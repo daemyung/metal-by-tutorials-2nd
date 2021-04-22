bca9d09f43a355e8d594bb4c1e38c0bf6d5b470a
daemyung jang <dm86.jang@gmail.com>
2021-04-22 12:25:45 +0900

    Fix a runtime error

--- 04-3d-transforms/starter/3DTransforms.playground/Sources/Utility.swift
+++ 04-3d-transforms/starter/3DTransforms.playground/Sources/Utility.swift
@@ -5,7 +5,7 @@ public typealias float4 = SIMD4<Float>
 
 public var device: MTLDevice!
 public let commandQueue = device.makeCommandQueue()!
-public let library = createLibrary()
+public let library = device.makeDefaultLibrary()!
 public let pipelineState = createPipelineState(library: library)
 
 public var lightGrayColor: float4 = [0.9, 0.9, 0.9, 1]
@@ -13,17 +13,6 @@ public var redColor: float4 = [1, 0, 0, 1]
 
 public func setupMetal() {
   
-}
-public func createLibrary() -> MTLLibrary {
-  var library: MTLLibrary?
-  do {
-    let path = Bundle.main.path(forResource: "Shaders", ofType: "metal")
-    let source = try String(contentsOfFile: path!, encoding: .utf8)
-    library = try device.makeLibrary(source: source, options: nil)
-  } catch let error as NSError {
-    fatalError("library error: " + error.description)
-  }
-  return library!
 }
 
 public func createPipelineState(library: MTLLibrary) -> MTLRenderPipelineState {
